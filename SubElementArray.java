e7f9485918ccfda1b4b9fbeaa7de6c9d8243c334
coolprateek147 <prateekpathak@live.in>
2022-05-10 22:56:32 +0500

    Sub-Elements-of-Array

---
+++ SubElementArray.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class SubElementArray {
+    public static void main(String args[]) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        
+        int[] arr = new int[n];
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = scn.nextInt();
+        }
+        
+        int lim = (1 << n);
+      //  System.out.println(lim);
+        
+        for(int i = 0; i < lim; i++){
+            int dec = i;
+            String str = "";
+            for(int j = 0; j < arr.length; j++){
+                int rem = dec % 2;
+                dec = dec / 2;
+                if(rem == 0){
+                    str = "-\t" + str;
+                } else {
+                    str = arr[arr.length - 1 - j] + "\t" + str;
+                }
+            }
+            System.out.println(str);
+        }
+        
+    }
+}
