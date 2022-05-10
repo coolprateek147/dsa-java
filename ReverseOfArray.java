8df95d33553c0bfa75a28a137326dc4474dae41f
coolprateek147 <prateekpathak@live.in>
2022-05-10 22:53:19 +0500

    ReverseOfArray

---
+++ ReverseOfArray.java
@@ -0,0 +1,27 @@
+public class ReverseOfArray {
+    public static void main(String args[]) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        
+        int[] arr = new int[n];
+        
+        for(int i = 0; i< arr.length; i++){
+            arr[i] = scn.nextInt();
+        }
+        
+        int left = 0;
+        int right = arr.length - 1;
+        
+        while(left < right){
+            temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }
+        
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
