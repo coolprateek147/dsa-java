5a4bd351dc8c4940d4a7c3ed9c1ff47647ea9d65
coolprateek147 <prateekpathak@live.in>
2022-05-10 22:54:04 +0500

    InverseOfArray

---
+++ InverseOfArray.java
@@ -0,0 +1,21 @@
+public class InverseOfArray {
+    public static void main(String args[]) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        
+        int[] arr = new int[n];
+        int[] inv = new int[n];
+        
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = scn.nextInt();
+        }
+        
+        for(int i = 0; i < inv.length; i++){
+            inv[arr[i]] = i;
+        }
+        
+        for(int i = 0; i < inv.length; i++){
+            System.out.print(inv[i] + " ");
+        }
+    }
+}
